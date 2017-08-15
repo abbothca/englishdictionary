a1ab75e98f20e4601feb1a07506a0a2da102c30f
Liubov Khomych <kroka.luba@gmail.com>
2017-08-15 15:32:59 +0300

    Dictionary: config

--- dictionary.config.js
+++ dictionary.config.js
@@ -1,6 +1,6 @@
 var config = {
-  oxforddictionariesAPIKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  oxforddictionariesAPIId: 'yyyyyyyy'
+  oxforddictionariesAPIKey: '00ae5e56da01facb4ceef0322954a6c4',
+  oxforddictionariesAPIId: 'f08cf9ea'
 };
 
 module.exports = config;
